6abb3537d7e71115c43002965f08b8ba2d1baa73
Maxim Vinokurov <mafiozzy101@gmail.com>
2020-09-08 14:50:37 +0300

    Добавил функционал с планетами

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+import ephem
 from mybot import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -20,6 +21,27 @@ def greet_user(update, context):
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
 
+def coord(update, context):
+    user_text = str(update.message.text.split()[1])
+    date = ephem._datetime.utcnow()
+    if user_text == 'mercury':
+        update.message.reply_text(ephem.constellation(ephem.Mercury(date)))
+    elif user_text == 'venus':
+        update.message.reply_text(ephem.constellation(ephem.Venus(date)))
+    elif user_text == 'mars':
+        update.message.reply_text(ephem.constellation(ephem.Mars(date)))
+    elif user_text == 'jupiter':
+        update.message.reply_text(ephem.constellation(ephem.Jupiter(date)))
+    elif user_text == 'saturn':
+        update.message.reply_text(ephem.constellation(ephem.Saturn(date)))
+    elif user_text == 'uranus':
+        update.message.reply_text(ephem.constellation(ephem.Uranus(date)))
+    elif user_text == 'neptune':
+        update.message.reply_text(ephem.constellation(ephem.Neptune(date)))
+    else:
+        update.message.reply_text('Запрос неверен.')
+
+
 def talk_to_me(update, context):
     user_text = update.message.text
     logging.info(user_text)
@@ -33,6 +55,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", coord))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал!')
